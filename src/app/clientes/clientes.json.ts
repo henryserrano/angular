543aafea46b96db006b493e3ce5772a24124c439
henryserrano <henryserrano@hotmail.com>
2020-05-12 00:10:01 +1000

    Cambio en usuario

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -5,7 +5,7 @@ import { Cliente } from "./cliente";
   {id:2,nombre:"Diana" , apellido:"Fernandez" ,email:"diana@gmail.com" ,createAt:"2020-05-05"},
   {id:3,nombre:"Juan" , apellido:"Gonzalez" ,email:"juan@gmail.com" ,createAt:"2020-11-07"},
   {id:4,nombre:"Carlos" , apellido:"Vargas" ,email:"carlos@gmail.com" ,createAt:"2020-02-05"},
-  {id:5,nombre:"Pedro" , apellido:"Serrano" ,email:"henryserrano@gmail.com" ,createAt:"2020-11-05"},
+  {id:5,nombre:"Pedro" , apellido:"Serrano" ,email:"pedritoserrano@gmail.com" ,createAt:"2020-11-05"},
   {id:6,nombre:"Camila" , apellido:"Fernandez" ,email:"diana@gmail.com" ,createAt:"2020-05-05"},
   {id:7,nombre:"Juan" , apellido:"Gonzalez" ,email:"juan@gmail.com" ,createAt:"2020-11-07"},
   {id:8,nombre:"Peter" , apellido:"Vargas" ,email:"carlos@gmail.com" ,createAt:"2020-02-05"},
