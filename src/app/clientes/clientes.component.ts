00fad77d532a2abd31f20e4207a886b4ceef9231
henryserrano <henryserrano@hotmail.com>
2020-05-20 00:32:34 +1000

    Crud ready

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import {ClienteService} from './cliente.service';
+import swal from 'sweetalert2';
 
 
 @Component({
@@ -17,11 +18,35 @@ ngOnInit() {
   this.clienteService.getClientes().subscribe(
     (clientes) => this.clientes=clientes
   );
+}
 
+delete(cliente: Cliente): void{
+  swal.fire({
+  title: 'Esta seguro?',
+  text: `Desea eliminar el usuario! ${cliente.nombre}`,
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Si, eliminar!'
+}).then((result) => {
+  if (result.value) {
+    this.clienteService.delete(cliente.id).subscribe(
+      response =>{
+        this.clientes=this.clientes.filter(cli =>cli !==cliente)
+        swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    )
 
-
+  }
+})
 
 }
+
 
 
 }
